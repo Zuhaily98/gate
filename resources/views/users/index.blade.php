2e633be6abe441f78dd2203bedc3cdef6bee514e
Zuhaily98 <zuhaily98@gmail.com>
2021-08-16 13:39:14 +0800

    Change view message

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -5,7 +5,7 @@
         <div class="row justify-content-center">
             <div class="col">
                 <div class="card">
-                    <div class="card-header">List of Users</div>
+                    <div class="card-header">List of All User</div>
 
                     <div class="card-body">
                         <table class="table">
